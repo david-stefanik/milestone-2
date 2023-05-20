b977d4dc43c869217718d3e5992828f3dc859fcc
Aphra Hiat <117119563+Aphraditti@users.noreply.github.com>
2023-05-20 12:04:23 -0700

    skeleton in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import path from 'path';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  const appString = ReactDOMServer.renderToString(<App />);
+  const indexHTML = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(indexHTML.replace(/\\/g, '/').replace(/\/index\.html$/, '/404.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
