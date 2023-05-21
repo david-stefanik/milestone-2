967e27e17d354b0511cdf27fa2bf93160c05fb5f
Aphra Hiat <117119563+Aphraditti@users.noreply.github.com>
2023-05-21 10:16:16 -0700

    Updated frontend files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './frontend/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 
+'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      "Hello World!"
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
