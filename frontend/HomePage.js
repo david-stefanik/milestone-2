3e699ff3f491f799216ebfc752e61d193f3e0a34
Jose Covarrubias <josecovarrubias@Joses-MacBook-Air.local>
2023-05-21 13:26:07 -0700

    adding Homepage

--- frontend/HomePage.js
+++ frontend/HomePage.js
@@ -1,17 +1,38 @@
+import { Button } from 'bootstrap';
 import React from 'react';
-import '../App.css';
-import './HomePage.css';
 
 
-function HomePage() {
+
+export default function HomePage() {
     return (
-        <div>
-        <P>HELLO WORLD</P>
-
+        <>
+        <div className='nav-bar'>
+          {/* nav link */}
         </div>
+            <div className='header'>
+                <h1>Header sweet sweet merch</h1>
+            </div>
+
+            <div className='blog-names-btn'>
+                <div className='btn-aphra'>
+                    <Button to=''> Aphra</Button>
+                </div>
+                <div className='btn-david'>
+                    <Button to=''> David</Button>
+                </div>
+                <div className='btn-aphra'>
+                    <Button to=''> Iman</Button>
+                </div>
+                <div className='btn-aphra'>
+                    <Button to=''> Jose</Button>
+                </div>
+
+
+            </div>
+
+            </>
     )
 }
 
 
 
-export default HomePage
--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from '../frontend/HomePage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      "Hello World!"
-      'hello'
+   <div className="App">
+    <HomePage />
     </div>
+
+  
   );
 }
 
