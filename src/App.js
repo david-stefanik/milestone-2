c7ea735011ea4352c212704a6bde6fa7b8e831bf
Jose Covarrubias <josecovarrubias@Joses-Air.lan>
2023-05-30 02:07:06 -0700

    Merch page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
-import navBar from './pages/navbar';
+// import NavBar from './components /navbar';
+import Merch from './components /merch';
+import { Routes, Route} from 'react-router-dom'
+import Cart from './components /Cart/Cart';
+import DataProvider from './components /Context/DataContext';
+
+
+
+
 
 
 
 function App() {
   return (
-   <div className="App">
-    <navBar />
-    </div>
+<DataProvider>
+<Routes>
+  <Route path='/' element={<Merch />}/>
+  <Route path='/cart' element={<Cart />}/>
+</Routes>
+</DataProvider>
 
   
   );
